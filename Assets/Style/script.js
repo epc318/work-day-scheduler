c6f5e01277a43d784e361f695ffc2af2eaa73aae
Rick Johnson <carl4917@umn.edu>
2021-11-21 14:02:57 -0600

    more js

--- Assets/Style/script.js
+++ Assets/Style/script.js
@@ -1,3 +1,43 @@
 // Display current date and day of the week
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // save text input to local storage
+        localStorage.setItem(time, text);
+    })
+
+       
+    function timeTracker() {
+        //current hour
+        var currentTime = moment().hour();
+
+        // looping through each time row
+        $(".time-row").each(function () {
+            var rowTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (rowTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (rowTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
